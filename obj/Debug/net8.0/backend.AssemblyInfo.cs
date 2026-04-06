bd01d9fda897de852629936853ac4f5c3e6ed347
isagrt <isadoragoedert05@gmail.com>
2026-04-06 11:57:29 -0300

    alterando e arrumando alguns erros

--- obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6395f1309ff26fcfb91b66fb66f1eb745d2874e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb248e80027a423c4e3a0753f85171b5c15be85c")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
